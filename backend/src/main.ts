62876775330e70eee97cce9c252c5e63d79843ee
Ankit-kumar <ankit.kumar@cloudflex.co.in>
2025-12-28 04:02:21 +0530

    deployment change (port fix)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -33,7 +33,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+const port = process.env.PORT || 3000;
+await app.listen(port);
   console.log('🚀 Server running on http://localhost:3000');
   console.log('📘 Swagger docs at http://localhost:3000/api');
 }
